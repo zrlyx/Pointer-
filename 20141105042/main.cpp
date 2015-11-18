25c8afb338be9552e6e82eb5d39db71f09ef55b6
zrlyx <20141105042hyb@admindeiMac-007.local>
2015-11-18 19:13:09 +0800

    指针
    第一次

--- 20141105042/main.cpp
+++ 20141105042/main.cpp
@@ -10,6 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    
+    printf("a=%x  &a=&x &p=%x p=%x\n",a,&a,&p,p,*p);
+    
     return 0;
 }
